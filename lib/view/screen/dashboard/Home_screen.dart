050b961067547d63f4c32106a65aa758fe00d924
RimonChakma <rimon.chakma880@gmail.com>
2024-10-30 11:07:22 +0600

    create search bar

--- lib/view/screen/dashboard/Home_screen.dart
+++ lib/view/screen/dashboard/Home_screen.dart
@@ -6,7 +6,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Home"),),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(onPressed: (){}, icon: Icon(Icons.qr_code_scanner,color: Colors.black,)),
+                  Expanded(
+                    child: Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.orange,width: 2),
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    
+                    
+                      child: Row(
+                        children: [
+                         
+                          Expanded(child: TextField(
+                            
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none
+                              ),
+                            
+                              prefixIcon: Icon(Icons.camera_alt_outlined),
+                              hintText: "Search Now",
+                              hintStyle: TextStyle(
+                                color: Colors.black.withOpacity(.4)
+                              ),
+                              suffixIcon: Icon(Icons.search),                           
+                            ),
+                          
+                          )),
+                         
+                        ],
+                      ),
+                    ),
+                    
+                  ),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.notifications))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
