e763f2b0c98a2687016fc8627d9617ae7d8e12bf
RimonChakma <rimon.chakma880@gmail.com>
2024-10-30 23:12:55 +0600

    use stack alignment

--- lib/view/screen/dashboard/Home_screen.dart
+++ lib/view/screen/dashboard/Home_screen.dart
@@ -16,9 +16,14 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
 
     List<String> bannerList=[
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1GVUcQwkfU17x-iOhsR5e6mkIQKxUGjzMQA&s",
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZKvNdCu_bfstFw5nxOD_5EDrbJ8x5A64Erw&s",
-      "https://img.freepik.com/premium-vector/e-commerce-shopping-website-banner-design-template_556845-302.jpg?semt=ais_hybrid"
+      "https://img.freepik.com/free-vector/flat-sale-banner-with-photo_23-2149026968.jpg",
+      "https://img.freepik.com/premium-psd/online-shopping-sale-icon-with-cart-bag-percentage-tag-dark-background-3d-render-concept-buy_492780-3252.jpg?semt=ais_hybrid",
+      "https://img.freepik.com/premium-psd/online-shopping-landing-page-website-concept-with-3d-mobile-phone-parcel-trolley-cart_598466-172.jpg"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgAoYE__Cs375IeBRa3WO9TqHiZ_vNspw0gA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRn6tFdy8rNuPLySXVv1YP-vPjbGao2IEYrHn-_nqR2xqYWlVG1ErBbpXYTn1IbvVgmls4&usqp=CAU",
+      "https://img.pikbest.com/wp/202346/conceptual-illustration-of-e-commerce-shopping-on-website-or-mobile-app-in-3d-render_9623895.jpg!bw700",
+      "https://png.pngtree.com/background/20231030/original/pngtree-3d-rendering-of-an-ecommerce-app-in-illustration-style-picture-image_5807880.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGVSy4nsXZlnrin-sSIBkKLRPXY11K5VddbvgAnZ6CioWzr20lDewv_Ts21Mf7HKONdyc&usqp=CAU"
     ];
     return Scaffold(
       body: Padding(
@@ -70,12 +75,40 @@ class _HomeScreenState extends State<HomeScreen> {
                          
                 items: [
                ...bannerList.map((value){
-                return Container(                
-                  decoration: BoxDecoration(                    
-                    borderRadius: BorderRadius.circular(30),
-                    color: Colors.black,
-                    image: DecorationImage(image: NetworkImage(value),fit: BoxFit.cover)               
+                return Stack(
+                  alignment: Alignment.bottomCenter,
+                  
+                  children:[
+                    Container(                
+                    decoration: BoxDecoration(                    
+                      borderRadius: BorderRadius.circular(30),
+                      color: Colors.black,
+                      image: DecorationImage(image: NetworkImage(value),fit: BoxFit.cover)               
+                    ),
                   ),
+
+                  Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+               ... bannerList.asMap().entries.map((value) {
+                  return  Container(
+                    
+                    margin: EdgeInsets.symmetric(vertical: 10,horizontal: 5),
+                    
+                    height: 6,
+                    width: 6,
+                    decoration: BoxDecoration(
+                      color: currentIndex == value.key? Colors.yellow:Colors.white,
+                      shape: BoxShape.circle
+                    ),
+                  );
+                },
+               )
+                ],
+              )
+
+                  ] 
+
                 );
                })
               ], options: CarouselOptions(
@@ -88,7 +121,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
                 height: 160,
                 autoPlay: true,
-                aspectRatio: 3/4,
+                aspectRatio: 6/20,
                 enlargeCenterPage: true,
                 autoPlayInterval: Duration(
                   seconds: 3
@@ -99,24 +132,7 @@ class _HomeScreenState extends State<HomeScreen> {
               )),
 
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-               ... bannerList.asMap().entries.map((value) {
-                  return  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10,horizontal: 5),
-                    
-                    height: 10,
-                    width: 10,
-                    decoration: BoxDecoration(
-                      color: currentIndex == value.key? Colors.yellow:Colors.blue,
-                      shape: BoxShape.circle
-                    ),
-                  );
-                },
-               )
-                ],
-              )
+              
             ],
 
 
